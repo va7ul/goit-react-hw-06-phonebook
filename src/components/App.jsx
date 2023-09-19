8741ab77c0114c6f1d4b0f3a955d501180f43d46
va7ul <vas7ul@gmail.com>
2023-09-19 16:37:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,7 @@ import { ContactForm } from './ContactForm/ContactForm.jsx';
 import { ContactList } from './ContactList/ContactList.jsx';
 import { Filter } from './Filter/Filter.jsx';
 
-// const getInitialContacts = () => {
-//   if (localStorage.getItem('contacts') !== null) {
-//     return JSON.parse(localStorage.getItem('contacts'));
-//   }
-//   return [];
-// };
-
 export const App = () => {
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <div>
       <h1>Phonebook</h1>
